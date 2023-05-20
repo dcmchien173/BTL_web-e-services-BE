3c8e9da63cbf3a6b42398d933dda45c5170662d2
admin <hoangchinhcva@gmail.com>
2023-05-20 11:09:19 +0700

    Creat Database

--- Project/QuizPractice/src/java/model/Quiz.java
+++ Project/QuizPractice/src/java/model/Quiz.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Project.src.java.model;
+package Project.QuizPractice.src.java.model;
 
 import java.sql.Date;
 
