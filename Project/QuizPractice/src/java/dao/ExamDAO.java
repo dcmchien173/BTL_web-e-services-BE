8bda6fe6abf118d6da37aae53e70bf6c97d5fa87
NGUYENVANCHIENBK65 <109352952+NGUYENVANCHIENBK65@users.noreply.github.com>
2023-06-13 18:48:59 +0700

    Add files via upload

--- Project/QuizPractice/src/java/dao/ExamDAO.java
+++ Project/QuizPractice/src/java/dao/ExamDAO.java
@@ -12,7 +12,7 @@ import model.Question;
 
 public class ExamDAO extends DBContext {
 
-    public List<Question> getNQuestion(int quizId, int numOfQ) {
+    public List<Question> getNQuestion(String quizId, int numOfQ) {
         List<Question> listQuesInExam = new ArrayList<>();
         String query = "select top (?) *from Question \n"
                 + "		where quiz_id = ?\n"
@@ -20,12 +20,12 @@ public class ExamDAO extends DBContext {
         try {
             PreparedStatement pd = connection.prepareStatement(query);
             pd.setInt(1, numOfQ);
-            pd.setInt(2, quizId);
+            pd.setString(2, quizId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 Question examQues = new Question();
-                int questionId = rs.getInt("question_id");
+                String questionId = rs.getString("question_id");
                 String question = rs.getString("question");
                 String instruction = rs.getString("intruction");
                 List<Option> listOp = new ExamDAO().GetOptionByQuestion(quizId, questionId);
@@ -43,14 +43,14 @@ public class ExamDAO extends DBContext {
         return listQuesInExam;
     }
 
-    public List<Option> GetOptionByQuestion(int quiz_id, int question_id) {
+    public List<Option> GetOptionByQuestion(String quiz_id, String question_id) {
         List<Option> list = new ArrayList<>();
         String query = " select * from [Option]\n"
                 + "  where quiz_id = ? and question_id = ?";
         try {
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quiz_id);
-            pd.setInt(2, question_id);
+            pd.setString(1, quiz_id);
+            pd.setString(2, question_id);
             ResultSet rs = pd.executeQuery();
             while (rs.next()) {
                 Option op = new Option();
@@ -67,12 +67,12 @@ public class ExamDAO extends DBContext {
         return list;
     }
     
-    public void addExam(int user_id, String quiz_id, float mark, int correct_anwers, int num_of_question, String date, String time) {
-        String query = "insert into exam (user_id, quiz_id, mark, correct_answers, num_of_question, exam_date,time)\n"
+    public void addExam(String Student_id, String quiz_id, float mark, int correct_anwers, int num_of_question, String date, String time) {
+        String query = "insert into exam (Student_id, quiz_id, mark, correct_answers, num_of_question, exam_date,time)\n"
                 + "values (?,?,?,?,?,?,?)";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
-            ps.setInt(1, user_id);
+            ps.setString(1, Student_id);
             ps.setString(2, quiz_id);
             ps.setFloat(3, mark);
             ps.setInt(4, correct_anwers);
@@ -86,13 +86,13 @@ public class ExamDAO extends DBContext {
         }
     }
 
-    public int getCurentExamID(int user_id, String quiz_id) {
+    public int getCurentExamID(String Student_id, String quiz_id) {
         try {
             String query = "select top(1) exam_id from exam \n"
-                    + "where user_id = ? and quiz_id = ? \n"
+                    + "where Student_id = ? and quiz_id = ? \n"
                     + "order by exam_id desc";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, user_id);
+            pd.setString(1, Student_id);
             pd.setString(2, quiz_id);
             ResultSet rs = pd.executeQuery();
             while (rs.next()) {
@@ -104,14 +104,14 @@ public class ExamDAO extends DBContext {
         return 0;
     }
 
-    public void addQuesExam(int exam_id, List<Question> questionReview) {
+    public void addQuesExam(String exam_id, List<Question> questionReview) {
         String query = "insert into Question_exam(exam_id, question_id, question, question_status)\n"
                 + "values (?, ?, ?, ?)";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
             for (Question question : questionReview) {
-                ps.setInt(1, exam_id);
-                ps.setInt(2, question.getQuestion_id());
+                ps.setString(1, exam_id);
+                ps.setString(2, question.getQuestion_id());
                 ps.setString(3, question.getQuestion());
                 ps.setInt(4, question.getCheckQuestion());
                 ps.executeUpdate();
@@ -120,14 +120,14 @@ public class ExamDAO extends DBContext {
         }
     }
 
-    public void addOptionExam(int exam_id, List<Option> optionReview) {
+    public void addOptionExam(String exam_id, List<Option> optionReview) {
         String query = "insert into Option_question_exam (exam_id,question_id,option_id,[option],right_option,option_status)\n"
                 + "values (?,?,?,?,?,?)";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
             for (Option option : optionReview) {
-                ps.setInt(1, exam_id);
-                ps.setInt(2, option.getQuestion_id());
+                ps.setString(1, exam_id);
+                ps.setString(2, option.getQuestion_id());
                 ps.setInt(3, option.getOption_id());
                 ps.setString(4, option.getOption_content());
                 ps.setInt(5, option.getRight_option());
@@ -139,15 +139,15 @@ public class ExamDAO extends DBContext {
 
     }
 
-    public String getQuizNamebyID(int quiz_id) {
+    public String getQuizNamebyID(String quiz_id) {
         try {
-            String query = "select q.[name] from Quiz q\n"
+            String query = "select q.[quiz_name] from Quiz q\n"
                     + "where q.quiz_id = ?";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quiz_id);
+            pd.setString(1, quiz_id);
             ResultSet rs = pd.executeQuery();
             while (rs.next()) {
-                return rs.getString("name");
+                return rs.getString("quiz_name");
             }
         } catch (SQLException e) {
             System.out.println(e);
@@ -155,23 +155,23 @@ public class ExamDAO extends DBContext {
         return null;
     }
 
-    public List<ExamReview> getExamReviewByUserId(int user_id) {
+    public List<ExamReview> getExamReviewByTeacherId(String Teacher_id) {
         List<ExamReview> list = new ArrayList<>();
         String query = "select * from exam e \n"
-                + "where e.user_id =?\n"
+                + "where e.Student_id =?\n"
                 + "order by exam_id desc";
         try {
 
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, user_id);
+            pd.setString(1, Teacher_id);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 ExamReview examReview = new ExamReview();
                 ExamDAO dao = new ExamDAO();
-                int exam_id = rs.getInt("exam_id");
+                String exam_id = rs.getString("exam_id");
 
-                int quiz_id = rs.getInt("quiz_id");
+                String quiz_id = rs.getString("quiz_id");
                 String quiz_name = dao.getQuizNamebyID(quiz_id);
                 float mark = rs.getFloat("mark");
                 int correct_answers = rs.getInt("correct_answers");
@@ -180,7 +180,7 @@ public class ExamDAO extends DBContext {
                 String time = rs.getString("time");
 
                 examReview.setExam_id(exam_id);
-                examReview.setUser_id(user_id);
+                examReview.setTeacher_id(Teacher_id);
                 examReview.setQuiz_id(quiz_id);
                 examReview.setQuiz_name(quiz_name);
                 examReview.setDate(date);
@@ -196,22 +196,22 @@ public class ExamDAO extends DBContext {
         return list;
     }
 
-    public List<ExamReview> getExamReviewByQuizID(int user_id, int quiz_id) {
+    public List<ExamReview> getExamReviewByQuizID(String Teacher_id, String quiz_id) {
         List<ExamReview> list = new ArrayList<>();
         String query = "select * from exam \n"
-                + "where user_id =? and quiz_id=?\n"
+                + "where Teacher_id =? and quiz_id=?\n"
                 + "order by exam_id desc";
         try {
 
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, user_id);
-            pd.setInt(2, quiz_id);
+            pd.setString(1, Teacher_id);
+            pd.setString(2, quiz_id);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 ExamReview examReview = new ExamReview();
                 ExamDAO dao = new ExamDAO();
-                int exam_id = rs.getInt("exam_id");
+                String exam_id = rs.getString("exam_id");
                 String quiz_name = dao.getQuizNamebyID(quiz_id);
                 float mark = rs.getFloat("mark");
                 int correct_answers = rs.getInt("correct_answers");
@@ -220,7 +220,7 @@ public class ExamDAO extends DBContext {
                 String time = rs.getString("time");
 
                 examReview.setExam_id(exam_id);
-                examReview.setUser_id(user_id);
+                examReview.setTeacher_id(Teacher_id);
                 examReview.setQuiz_id(quiz_id);
                 examReview.setQuiz_name(quiz_name);
                 examReview.setDate(date);
@@ -241,7 +241,7 @@ public class ExamDAO extends DBContext {
         for (int i = 0; i < listExam.length; i++) {
             Option ex = new Option();
             String[] words = listExam[i].split("\\-");
-            ex.setQuestion_id(Integer.parseInt(words[0]));
+            ex.setQuestion_id(words[0]);
             ex.setOption_id(Integer.parseInt(words[1]));
             ex.setRight_option(Integer.parseInt(words[2]));
             int start = words[0].length() + words[1].length() + words[2].length() + 3;
@@ -259,12 +259,12 @@ public class ExamDAO extends DBContext {
             boolean chekQues = true;
             String[] words = listQues[i].split("\\-");   // word 0 là question id
             for (Option opReview : optionReview) {
-                if (opReview.getQuestion_id() == Integer.parseInt(words[0]) && opReview.getOption_status() != opReview.getRight_option()) {
+                if (opReview.getQuestion_id() == words[0] && opReview.getOption_status() != opReview.getRight_option()) {
                     chekQues = false;
                 }
             }
             Question ques = new Question();
-            ques.setQuestion_id(Integer.parseInt(words[0]));
+            ques.setQuestion_id(words[0]);
             ques.setQuestion(listQues[i].substring(words[0].length() + 1));
             if (chekQues) {
                 ques.setCheckQuestion(1);
@@ -286,28 +286,28 @@ public class ExamDAO extends DBContext {
         return count;
     }
 
-    public ExamReview getExamById(int examId) {
+    public ExamReview getExamById(String examId) {
         ExamReview examReview = new ExamReview();
         String query = "select * from exam \n"
                 + "where exam_id = ?";
         try {
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, examId);
+            pd.setString(1, examId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 ExamDAO dao = new ExamDAO();
-                int quiz_id = rs.getInt("quiz_id");
+                String quiz_id = rs.getString("quiz_id");
                 String quiz_name = dao.getQuizNamebyID(quiz_id);
                 float mark = rs.getFloat("mark");
-                int user_id = rs.getInt("user_id");
+                String Teacher_id = rs.getString("Teacher_id");
                 int correct_answers = rs.getInt("correct_answers");
                 int num_of_question = rs.getInt("num_of_question");
                 String date = rs.getString("exam_date");
                 String time = rs.getString("time");
 
                 examReview.setExam_id(examId);
-                examReview.setUser_id(user_id);
+                examReview.setTeacher_id(Teacher_id);
                 examReview.setQuiz_id(quiz_id);
                 examReview.setQuiz_name(quiz_name);
                 examReview.setDate(date);
@@ -322,20 +322,20 @@ public class ExamDAO extends DBContext {
         return examReview;
     }
 
-    public List<Question> getQuesInExam(int exam_id) {
+    public List<Question> getQuesInExam(String exam_id) {
         List<Question> listQues = new ArrayList<>();
         String query = " select question_id, question,question_status from Question_exam  where exam_id = ? ";
         try {
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, exam_id);
+            pd.setString(1, exam_id);
             ResultSet rs = pd.executeQuery();
             while (rs.next()) {
                 Question ques = new Question();
-                ques.setQuestion_id(rs.getInt("question_id"));
+                ques.setQuestion_id(rs.getString("question_id"));
                 ques.setQuestion(rs.getString("question"));
                 ques.setCheckQuestion(rs.getInt("question_status"));
                 ExamDAO examDAO = new ExamDAO();
-                List<Option> listOption = examDAO.getOptionInExam(exam_id, rs.getInt("question_id"));
+                List<Option> listOption = examDAO.getOptionInExam(exam_id, rs.getString("question_id"));
                 ques.setListOption(listOption);
                 listQues.add(ques);
             }
@@ -344,15 +344,15 @@ public class ExamDAO extends DBContext {
         return listQues;
     }
 
-    public List<Option> getOptionInExam(int exam_id, int question_id) {
+    public List<Option> getOptionInExam(String exam_id, String question_id) {
         List<Option> listOp = new ArrayList<>();
         String query = "select question_id, option_id,[option], right_option, option_status \n"
                 + "from Option_question_exam  \n"
                 + "where exam_id = ? and question_id = ?";
         try {
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, exam_id);
-            pd.setInt(2, question_id);
+            pd.setString(1, exam_id);
+            pd.setString(2, question_id);
             ResultSet rs = pd.executeQuery();
             while (rs.next()) {
                 Option option = new Option();
@@ -370,7 +370,7 @@ public class ExamDAO extends DBContext {
     public static void main(String[] args) {
         ExamDAO dao = new ExamDAO();
         List<Option> listop = new ArrayList<>();
-        listop = dao.GetOptionByQuestion(37, 9);
+        listop = dao.GetOptionByQuestion("37", "9");
         for (Option option : listop) {
             System.out.println(option);
         }
