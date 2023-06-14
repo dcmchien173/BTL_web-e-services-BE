a26a50cdfb7a15eed8784f4ff1de69bda50a4e1c
NGUYENVANCHIENBK65 <109352952+NGUYENVANCHIENBK65@users.noreply.github.com>
2023-06-14 11:00:52 +0700

    Add files via upload

--- Project/QuizPractice/src/java/dao/QuestionDAO.java
+++ Project/QuizPractice/src/java/dao/QuestionDAO.java
@@ -9,18 +9,18 @@ import model.Option;
 import model.Question;
 
 public class QuestionDAO extends DBContext {
-    public List<Question> getAllQuestion(int quizId) {
+    public List<Question> getAllQuestion(String quizId) {
         List<Question> list = new ArrayList<>();
         try {
             String query = "select* from Question\n"
                     + "where quiz_id= ? ";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quizId);
+            pd.setString(1, quizId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 Question q = new Question();
-                int questionId = rs.getInt("question_id");
+                String questionId = rs.getString("question_id");
                 String question = rs.getString("question");
                 String intruction = rs.getString("intruction");
                 q.setQuiz_id(quizId);
@@ -35,7 +35,7 @@ public class QuestionDAO extends DBContext {
         return list;
     }
 
-    public List<Question> getNQuestion(int quizId, int numOfQ) {
+    public List<Question> getNQuestion(String quizId, int numOfQ) {
         List<Question> list = new ArrayList<>();
         String query = "select top (?) *from Question \n"
                 + "		where quiz_id = ?\n"
@@ -44,12 +44,12 @@ public class QuestionDAO extends DBContext {
 
             PreparedStatement pd = connection.prepareStatement(query);
             pd.setInt(1, numOfQ);
-            pd.setInt(2, quizId);
+            pd.setString(2, quizId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 Question q = new Question();
-                int questionId = rs.getInt("question_id");
+                String questionId = rs.getString("question_id");
                 String question = rs.getString("question");
                 String intruction = rs.getString("intruction");
                 q.setQuiz_id(quizId);
@@ -64,18 +64,18 @@ public class QuestionDAO extends DBContext {
         return list;
     }
 
-    public List<Option> getAllOption(int quizId) {
+    public List<Option> getAllOption(String quizId) {
         List<Option> list = new ArrayList<>();
         try {
             String query = "select * from [Option] \n"
                     + "where quiz_id = ? ";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quizId);
+            pd.setString(1, quizId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 Option option = new Option();
-                int question_id = rs.getInt("question_id");
+                String question_id = rs.getString("question_id");
                 int option_id = rs.getInt("option_id");
                 int right_option = rs.getInt("right_option");
                 String Option_content = rs.getString("Option_content");
@@ -93,13 +93,13 @@ public class QuestionDAO extends DBContext {
         return list;
     }
 
-    public void addQuestion(int last_ID, int question_id, String question, String instruction) {
+    public void addQuestion(String last_ID, String question_id, String question, String instruction) {
         Question q = new Question();
         String query = "insert into Question(quiz_id, question_id, question, intruction) values(?, ?, ?, ?)";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
-            ps.setInt(1, last_ID);
-            ps.setInt(2, question_id); //lay quiz_id ma nguoi dung vua tao
+            ps.setString(1, last_ID);
+            ps.setString(2, question_id); //lay quiz_id ma nguoi dung vua tao
             ps.setString(3, question);
             ps.setString(4, instruction);
             ps.executeUpdate();
@@ -109,14 +109,14 @@ public class QuestionDAO extends DBContext {
         }
     }
 
-    public void addOption(int quiz_id, int question_id, int option_id, int right_option, String option_content) {
+    public void addOption(String quiz_id, String question_id, int option_id, int right_option, String option_content) {
         Option option = new Option();
         String query = "insert into [Option](quiz_id,question_id, option_id, right_option, option_content)\n"
                 + "values(?, ?, ?, ?, ?)";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
-            ps.setInt(1, quiz_id);
-            ps.setInt(2, question_id); //lay quiz_id ma nguoi dung vua tao
+            ps.setString(1, quiz_id);
+            ps.setString(2, question_id); //lay quiz_id ma nguoi dung vua tao
             ps.setInt(3, option_id);
             ps.setInt(4, right_option);
             ps.setString(5, option_content);
@@ -126,30 +126,30 @@ public class QuestionDAO extends DBContext {
         }
     }
     
-    public void updateQuestion(String question, String instruction, int question_id, int quiz_id) {
+    public void updateQuestion(String question, String instruction, String question_id, String quiz_id) {
         String query = "update Question set question=?, intruction=?\n"
                 + "where question_id = ? and quiz_id=?";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
             ps.setString(1, question);
             ps.setString(2, instruction);
-            ps.setInt(3, question_id);
-            ps.setInt(4, quiz_id);
+            ps.setString(3, question_id);
+            ps.setString(4, quiz_id);
             ps.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void updateOption(int right_option, String option_content, int question_id, int quiz_id, int option_id) {
+    public void updateOption(int right_option, String option_content, String question_id, String quiz_id, int option_id) {
         String query = "update [Option] set right_option=?, option_content=? \n"
                 + "where question_id = ? and quiz_id=? and option_id=?";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
             ps.setInt(1, right_option);
             ps.setString(2, option_content);
-            ps.setInt(3, question_id);
-            ps.setInt(4, quiz_id);
+            ps.setString(3, question_id);
+            ps.setString(4, quiz_id);
             ps.setInt(5, option_id);
             ps.executeUpdate();
         } catch (Exception e) {
@@ -157,37 +157,37 @@ public class QuestionDAO extends DBContext {
         }
     }
     
-    public void deleteQuestion(int quiz_id) {
+    public void deleteQuestion(String quiz_id) {
         String query = "Delete from Question where quiz_id= ?";
         
         try {
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quiz_id);
+            pd.setString(1, quiz_id);
             pd.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
     
-    public void deleteOption(int quiz_id) {
+    public void deleteOption(String quiz_id) {
         String query = "Delete from [Option] where quiz_id = ?";
         
         try {
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quiz_id);
+            pd.setString(1, quiz_id);
            pd.executeUpdate();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
     
-    public Question getQuestionByID(int quiz_id,int questionId) {
+    public Question getQuestionByID(String quiz_id, String questionId) {
         Question q = new Question();
         try {
             String query = "select * from Question where quiz_id=? and question_id=?";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quiz_id);
-            pd.setInt(2, questionId);
+            pd.setString(1, quiz_id);
+            pd.setString(2, questionId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
@@ -204,14 +204,14 @@ public class QuestionDAO extends DBContext {
         return q;
     }
     
-    public List<Option> getOptionByID(int quizId,int questionId) {
+    public List<Option> getOptionByID(String quizId, String questionId) {
         List<Option> list = new ArrayList<>();
         try {
             String query = "select * from [Option] \n"
                     + "where quiz_id = ? and question_id = ?";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quizId);
-            pd.setInt(2, questionId);
+            pd.setString(1, quizId);
+            pd.setString(2, questionId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
@@ -233,17 +233,17 @@ public class QuestionDAO extends DBContext {
         return list;
     }
     
-    public List<Question> getRandomQuestion(int quizId) {
+    public List<Question> getRandomQuestion(String quizId) {
         List<Question> list = new ArrayList<>();
         try {
             String query = "select top 1 quiz.quiz_id,question_id,question,[name] from Question ques join Quiz quiz on ques.quiz_id=quiz.quiz_id where ques.quiz_id=? order by NEWID()";
             PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, quizId);
+            pd.setString(1, quizId);
             ResultSet rs = pd.executeQuery();
 
             while (rs.next()) {
                 Question q = new Question();
-                int questionId = rs.getInt("question_id");
+                String questionId = rs.getString("question_id");
                 String question = rs.getString("question");
                 String name = rs.getString("name");
                 q.setQuiz_id(quizId);
@@ -258,121 +258,9 @@ public class QuestionDAO extends DBContext {
         return list;
     }
     
-    public List<Question> getBoughtInstruction(int userId) {
-        List<Question> list = new ArrayList<>();
-        try {
-            String query = "select bi.user_id,q.name,bi.question_id, bi.question, bi.instruction \n"
-                    + "from Bought_instruction bi join Quiz q ON bi.quiz_id = q.quiz_id\n"
-                    + "where bi.user_id = ? ";
-            PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, userId);
-            ResultSet rs = pd.executeQuery();
-
-            while (rs.next()) {
-                Question q = new Question();
-                int questionId = rs.getInt("question_id");
-                String question = rs.getString("question");
-                String instruction = rs.getString("instruction");
-                String quizName = rs.getString("name");
-                q.setQuestion_id(questionId);
-                q.setQuestion(question);
-                q.setName(quizName);
-                q.setInstruction(instruction);
-                list.add(q);
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return list;
-    }
-
-    public List<Question> getQuizOfBoughtInstruction(int userId) {
-        List<Question> list = new ArrayList<>();
-        try {
-            String query = "select distinct bi.user_id,q.name,q.quiz_id \n"
-                    + "from Bought_instruction bi join Quiz q ON bi.quiz_id = q.quiz_id\n"
-                    + "where bi.user_id = ?";
-            PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, userId);
-            ResultSet rs = pd.executeQuery();
-            while (rs.next()) {
-
-                Question q = new Question();
-                int quizId = rs.getInt("quiz_id");
-                String quizName = rs.getString("name");
-                q.setName(quizName);
-                q.setQuiz_id(quizId);
-                list.add(q);
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return list;
-    }
-
-    public List<Question> getQuestionOfQuizOfBoughtInstruction(int userId, String quiz_id) {
-        List<Question> list = new ArrayList<>();
-        try {
-            String query = "select q.name,q.quiz_id,bi.question,bi.instruction,bi.question_id \n"
-                    + "from Bought_instruction bi join Quiz q ON bi.quiz_id = q.quiz_id\n"
-                    + "where bi.user_id = ? and bi.quiz_id = ?";
-            PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, userId);
-            pd.setString(2, quiz_id);
-            ResultSet rs = pd.executeQuery();
-
-            while (rs.next()) {
-                Question q = new Question();
-                int questionId = rs.getInt("question_id");
-                String question = rs.getString("question");
-                String instruction = rs.getString("instruction");
-                String quizName = rs.getString("name");
-                q.setQuestion_id(questionId);
-                q.setQuestion(question);
-                q.setName(quizName);
-                q.setInstruction(instruction);
-                list.add(q);
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return list;
-    }
-
-    public List<Option> getBoughtOption(int user_id) {
-        List<Option> list = new ArrayList<>();
-        try {
-            String query = "select bi.quiz_id,bi.user_id,bo.question_id,bo.option_id,bo.option_content,bo.right_option\n"
-                    + "from Bought_option bo join Bought_instruction bi\n"
-                    + "on bo.question_id = bi.question_id where bi.user_id = ?";
-            PreparedStatement pd = connection.prepareStatement(query);
-            pd.setInt(1, user_id);
-            ResultSet rs = pd.executeQuery();
-
-            while (rs.next()) {
-                Option option = new Option();
-                int quizId = rs.getInt("quiz_id");
-                int question_id = rs.getInt("question_id");
-                int option_id = rs.getInt("option_id");
-                int right_option = rs.getInt("right_option");
-                String Option_content = rs.getString("Option_content");
-
-                option.setQuiz_id(quizId);
-                option.setQuestion_id(question_id);
-                option.setOption_id(option_id);
-                option.setRight_option(right_option);
-                option.setOption_content(Option_content);
-                list.add(option);
-            }
-        } catch (SQLException e) {
-            System.err.println(e);
-        }
-        return list;
-    }
-
     public static void main(String[] args) {
         QuestionDAO dao = new QuestionDAO();
-        List<Question> list = dao.getNQuestion(25, 7);
+        List<Question> list = dao.getNQuestion("25", 7);
         for (Question question : list) {
             System.out.println(question);
         }
